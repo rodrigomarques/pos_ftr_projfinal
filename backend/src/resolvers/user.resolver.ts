277b9c5504bdc457756e5d6cd2d50012fd741723
Rodrigo Marques <marques.coti@gmail.com>
2026-02-21 09:57:42 -0300

    Change USER output

--- backend/src/resolvers/user.resolver.ts
+++ backend/src/resolvers/user.resolver.ts
@@ -1,26 +1,32 @@
-import { Arg, Ctx, Mutation, Query, Resolver, UseMiddleware } from 'type-graphql'
+import { Arg, Mutation, Query, Resolver, UseMiddleware } from 'type-graphql'
 import { UserModel } from '../models/user.model'
 import { UserService } from '@/services/user.service'
 import { CreateUserInput } from '@/dtos/input/user'
+import { IsAuth } from '@/middlewares/auth.middleware'
+import { UserOutput } from '@/dtos/output/user'
 
 @Resolver(() => UserModel)
 export class UserResolver {
   private userService = new UserService()
   
-  @Mutation(() => UserModel)
+  @Mutation(() => UserOutput)
   async createUser(
     @Arg('data', () => CreateUserInput) data: CreateUserInput
   ) {
     return this.userService.createUser(data)
   }
 
-  @Query(() => UserModel)
-  async getUser(@Arg('id', () => String) id: string): Promise<UserModel> {
+  @Query(() => UserOutput)
+  @UseMiddleware(IsAuth)
+  async getUser(
+    @Arg('id', () => String) id: string
+  ): Promise<UserOutput> {
     return this.userService.findUser(id)
   }
-
-  @Query(() => [UserModel])
-  async listUsers(): Promise<UserModel[]> {
+  
+  @Query(() => [UserOutput])
+  @UseMiddleware(IsAuth)
+  async listUsers(): Promise<UserOutput[]> {
     return this.userService.listUsers()
   }
 }
